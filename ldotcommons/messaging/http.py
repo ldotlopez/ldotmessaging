c43ea07cbf847c5511cdce20def7ff810f988fb1
Luis Lopez <luis@cuarentaydos.com>
2014-09-30 14:56:49 +0200

    Fix cache_delta bug

--- ldotcommons/messaging/http.py
+++ ldotcommons/messaging/http.py
@@ -5,11 +5,12 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86) Home software (KHTML, like Gecko)'
 
 
 class Http(messaging.Source):
-    def __init__(self, url):
+    def __init__(self, url, cache_delta=60):
         self._url = url
 
         self._fetcher = fetchers.UrllibFetcher(
             cache=True,
+            cache_delta=cache_delta,
             headers={'User-Agent': USER_AGENT})
         self._m = None
 
