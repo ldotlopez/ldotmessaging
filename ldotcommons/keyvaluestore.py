abe2b4533a418127b5a3bfaa29f189af8ec681fb
Luis López <ldotlopez@gmail.com>
2015-04-01 14:22:36 +0200

    Solve some issues with KeyValueStores

--- ldotcommons/keyvaluestore.py
+++ ldotcommons/keyvaluestore.py
@@ -13,19 +13,42 @@ class KeyValueItem(Base):
 
     key = Column(String, name='key', primary_key=True,
                  unique=True, nullable=False)
-    value_ = Column(String, name='value')
-    typ = Column(String(), name='type', default='string', nullable=False)
+    _value = Column(String, name='value')
+    _typ = Column(String(), name='type', default='str', nullable=False)
+
     _resolved = _UNDEF
 
     def __init__(self, key, value, typ=None):
-        if isinstance(value, bool):
+        self.key = key
+        self._type, self._value = self._native_to_internal(value)
+        if typ:
+            self._typ = typ
+
+    @property
+    def value(self):
+        return self._interal_to_native(self._typ, self._value)
+
+    @value.setter
+    def value(self, v):
+        self._typ, self._value = self._native_to_internal(v)
+
+    @staticmethod
+    def _native_to_internal(value):
+        if isinstance(value, str):
+            typ = 'str'
+
+        elif isinstance(value, bool):
             typ = 'bool'
+            value = '1' if value else '0'
+
         elif isinstance(value, int):
             typ = 'int'
+            value = str(value)
+
         elif isinstance(value, float):
             typ = 'float'
-        elif isinstance(value, str):
-            typ = 'str'
+            value = str(value)
+
         else:
             try:
                 value = json.dumps(value)
@@ -35,47 +58,41 @@ class KeyValueItem(Base):
                 value = pickle.dumps(value)
                 typ = 'pickle'
 
-        self.key = key
-        self.value_ = value
-        self.typ = typ
+        return (typ, value)
 
-    @property
-    def value(self):
-        if self._resolved == _UNDEF:
-            if self.typ == 'bool':
-                self._resolved = (self.value_ == '1')
+    @staticmethod
+    def _interal_to_native(typ, value):
+        if typ == 'bool':
+            return (value != '0')
 
-            elif self.typ == 'int':
-                self._resolved = int(self.value_)
+        elif typ == 'int':
+            return int(value)
 
-            elif self.typ == 'float':
-                self._resolved = float(self.value_)
+        elif typ == 'float':
+            return float(value)
 
-            elif self.typ == 'str':
-                self._resolved = str(self.value_)
+        elif typ == 'str':
+            return str(value)
 
-            elif self.typ == 'json':
-                self._resolved = json.loads(self.value_)
+        elif typ == 'json':
+            return json.loads(value)
 
-            elif self.typ == 'pickle':
-                self._resolved = pickle.loads(self.value_)
+        elif typ == 'pickle':
+            return pickle.loads(value)
 
-        return self._resolved
-
-    @value.setter
-    def value(self, v):
-        self.value_ = v
+        raise ValueError((typ, value))
 
 
 class KeyValueStore:
-    def __init__(self, store_path, separator='.'):
-
-        self._sess = create_session('sqlite:///'+path.realpath(store_path))
-        self.query = self._sess.query(KeyValueItem)
+    def __init__(self, session, separator='.'):
+        KeyValueItem.metadata.bind = session.get_bind()
+        KeyValueItem.metadata.create_all()
+        self._sess = session
+        self._query = self._sess.query(KeyValueItem)
 
     def get(self, k, default=_UNDEF):
         try:
-            item = self.query.filter(KeyValueItem.key == k).one()
+            item = self._query.filter(KeyValueItem.key == k).one()
         except exc.NoResultFound:
             if default is _UNDEF:
                 raise KeyError(k)
@@ -86,7 +103,7 @@ class KeyValueStore:
 
     def set(self, k, v):
         try:
-            item = self.query.filter(KeyValueItem.key == k).one()
+            item = self._query.filter(KeyValueItem.key == k).one()
             item.value = v
         except exc.NoResultFound:
             item = KeyValueItem(key=k, value=v)
@@ -109,7 +126,13 @@ class KeyValueStore:
             self.query.filter(KeyValueItem.key.startswith(k+".")))
 
 
+class KeyValueStorePath(KeyValueStore):
+    def __init__(self, store_path, *args, **kwargs):
+        sess = create_session('sqlite:///'+path.realpath(store_path))
+        super(KeyValueStorePath, self).__init__(session=sess, **kwargs)
+
+
 class KeyValueMemory(KeyValueStore):
     def __init__(self, separator='.'):
-        super(KeyValueMemory, self).__init__(
-            path=':memory:', separator=separator)
+        sess = create_session('sqlite:///:memory:')
+        super(KeyValueMemory, self).__init__(sess, separator=separator)
