dc61880ed6934e97453fc4c9e69b1e6c72b5d0df
Luis López <ldotlopez@gmail.com>
2015-05-13 14:21:17 +0200

    Store prototype

---
+++ ldotcommons/store.py
@@ -0,0 +1,82 @@
+class RecursiveDict(dict):
+    def __init__(self, d={}):
+        super(RecursiveDict, self).__init__()
+        for (k, v) in d.items():
+            self.__setitem__(k, v)
+
+    def set(self, key, value):
+        return self.__setitem__(key, value)
+
+    def get(self, key):
+        return self.__getitem__(key)
+
+    def delete(self, key):
+        return self.__delitem__(key)
+
+    def __setitem__(self, key, value):
+        store = super(RecursiveDict, self)
+
+        if '.' not in key:
+            store.__setitem__(key, value)
+
+        else:
+            key, subkey = key.split('.', 1)
+
+            if key not in self:
+                store.__setitem__(key, RecursiveDict({subkey: value}))
+
+            else:
+                store.__getitem__(key).__setitem__(subkey, value)
+
+    def __getitem__(self, key):
+        store = super(RecursiveDict, self)
+
+        if '.' not in key:
+            return store.__getitem__(key)
+
+        else:
+            key, subkey = key.split('.', 1)
+            substore = store.__getitem__(key)
+
+            try:
+                return substore.__getitem__(subkey)
+            except KeyError as e:
+                raise KeyError(key + '.' + e.args[0])
+
+    def __delitem__(self, key):
+        store = super(RecursiveDict, self)
+
+        if '.' not in key:
+            store.__delitem__(key)
+
+        else:
+            key, subkey = key.split('.', 1)
+            store.__getitem__(key).__delitem__(subkey)
+
+    def __contains__(self, key):
+        store = super(RecursiveDict, self)
+
+        if '.' not in key:
+            return store.__contains__(key)
+
+        else:
+            key, subkey = key.split('.', 1)
+            try:
+                return store.__getitem__(key).__contains__(subkey)
+            except KeyError:
+                return False
+
+
+class Store(RecursiveDict):
+    def __getattr__(self, attr):
+        rd = super(Store, self)
+
+        if attr not in self:
+            rd.__setitem__(attr, Store())
+
+        return rd.__getitem__(attr)
+
+    def __setattr__(self, attr, value):
+        rd = super(Store, self)
+
+        return rd.__setitem__(attr, value)
---
+++ tests/store.py
@@ -0,0 +1,126 @@
+import unittest
+from ldotcommons import store
+
+
+class TestRecursiveDict(unittest.TestCase):
+    def test_init(self):
+        rd = store.RecursiveDict({
+            'x': 1,
+            'y': 2,
+            'foo.bar': 3.14
+        })
+        self.assertEqual(rd['x'], 1)
+        self.assertEqual(rd['foo.bar'], 3.14)
+        with self.assertRaises(KeyError):
+            rd['a.b.c']
+
+        rd = store.RecursiveDict()
+        rd['x'] = 1
+        self.assertEqual(rd['x'], 1)
+        with self.assertRaises(KeyError):
+            rd['a.b.c']
+
+    def test_setget(self):
+        rd = store.RecursiveDict()
+        rd['x'] = 1
+
+        self.assertEqual(rd['x'], 1)
+        with self.assertRaises(KeyError):
+            rd['y']
+
+    def test_update(self):
+        rd = store.RecursiveDict({
+            'x': 1,
+            'y': 2,
+            'foo.bar': 3,
+            'foo.raz': 4
+        })
+
+        rd['x'] = 'x'
+        rd['foo.raz'] = 'y'
+
+        self.assertEqual(rd, {
+            'x': 'x',
+            'y': 2,
+            'foo': {
+                'bar': 3,
+                'raz': 'y'
+            }
+        })
+
+    def test_delete(self):
+        rd = store.RecursiveDict({
+            'x': 1,
+            'y': 2,
+            'foo.bar': 3,
+            'foo.raz': 4,
+            'z.y.a': 5,
+            'z.y.b': 6,
+            'z.y.c': 7,
+        })
+        del rd['z.y.c']
+        del rd['foo']
+        self.assertEqual(rd, {
+            'x': 1,
+            'y': 2,
+            'z': {
+                'y': {
+                    'a': 5,
+                    'b': 6,
+                }
+            }
+        })
+
+    def test_contains(self):
+        rd = store.RecursiveDict({
+            'x': 1,
+            'y': 2,
+            'foo.bar': 3,
+            'foo.raz': 4,
+            'z.y.a': 5,
+            'z.y.b': 6,
+            'z.y.c': 7,
+        })
+        self.assertTrue('x' in rd)
+        self.assertTrue('z.y.a' in rd)
+        self.assertFalse('bar' in rd)
+
+    def test_keyerror(self):
+        rd = store.RecursiveDict({
+            'x.y.z': None
+        })
+        with self.assertRaises(KeyError) as e:
+            rd['x.y.foo']
+        self.assertEqual(e.exception.args[0], 'x.y.foo')
+
+        with self.assertRaises(KeyError) as e:
+            rd['a.b.c']
+        self.assertEqual(e.exception.args[0], 'a')
+
+
+class TestStore(unittest.TestCase):
+    def test_simple(self):
+        x = store.Store()
+        x.foo = 1
+        x.bar.odd = 'x'
+
+        self.assertEqual(x.foo, 1)
+        self.assertEqual(x.bar.odd, 'x')
+
+
+class TestValidatedStore(unittest.TestCase):
+    def test_basic_schema(self):
+        def validator(k, v):
+            print("{}: {}".format(k, v))
+            return True
+
+        import ipdb; ipdb.set_trace()
+        foo = store.ValidatedStore(validator=validator)
+        foo['a_int'] = 1
+        foo['b_int'] = 1
+        foo['point.x'] = 1.0
+        foo['complex.real'] = 1
+        foo['complex.imaginary'] = 2
+
+if __name__ == '__main__':
+    unittest.main()
