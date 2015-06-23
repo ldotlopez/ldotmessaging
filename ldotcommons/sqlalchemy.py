3fa02b94bedb9e001d3622749eea04dd70457cd8
Luis López <lopezl@uji.es>
2015-06-23 09:30:13 +0200

    Cleanup code, enhaced glob_to_like

--- ldotcommons/sqlalchemy.py
+++ ldotcommons/sqlalchemy.py
@@ -16,17 +16,17 @@ def _re_fn(regexp, other):
     return re.search(regexp, other, re.IGNORECASE) is not None
 
 
-#@property
-#def __monkeypatch_Base_query(self):
-#    return self.session.query(self)
-
-
 def create_engine(uri='sqlite:///:memory:', echo=False, dburi=None):
     if dburi:
         warnings.warn('Usage of dburi is deprecated, use uri instead')
         uri = dburi
 
     engine = sa.create_engine(uri, echo=echo)
+
+    # @property
+    # def __monkeypatch_Base_query(self):
+    #     return self.session.query(self)
+
     # Base.metadata.create_all(bind=engine)
 
     # Monkeypatch magic
@@ -34,7 +34,7 @@ def create_engine(uri='sqlite:///:memory:', echo=False, dburi=None):
     # setattr(Base, 'session', create_session(engine=engine))
     # setattr(Base.__class__, 'query', __monkeypatch_Base_query)
 
-    # Enable regexp functionality for sqlite connections
+    # Enable regexp and foreign_keys functionality for sqlite connections
     if uri.startswith('sqlite:///'):
         @event.listens_for(engine, "begin")
         def do_begin(conn):
@@ -65,19 +65,20 @@ def create_session(uri=None, engine=None, echo=False, dburi=None):
 
 def query_from_params(conn, mapping, **params):
 
-    # This code can be used for add type-safety to this function
-    #from sqlalchemy.sql import sqltypes
-    #columns = self._mapping.__table__.columns
-    #for (colname, column) in columns.items():
-    #    columntype = column.type
-    ##     if isinstance(columntype, sqltypes.String):
-    #        self._ops[(colname, None)] = self.by_string
-    #        self._ops[(colname, 'like')] = self.by_string_like
-    #        self._ops[(colname, 'regexp')] = self.by_string_regexp
-    #
-    #    if isinstance(columntype, sqltypes.Integer):
-    #        self._ops[(colname, 'min')] = self.by_amount_min
-    #        self._ops[(colname, 'max')] = self.by_amount_max
+    # # This code can be used for add type-safety to this function
+    # from sqlalchemy.sql import sqltypes
+
+    # columns = self._mapping.__table__.columns
+    # for (colname, column) in columns.items():
+    #     columntype = column.type
+    #     if isinstance(columntype, sqltypes.String):
+    #         self._ops[(colname, None)] = self.by_string
+    #         self._ops[(colname, 'like')] = self.by_string_like
+    #         self._ops[(colname, 'regexp')] = self.by_string_regexp
+
+    #     if isinstance(columntype, sqltypes.Integer):
+    #         self._ops[(colname, 'min')] = self.by_amount_min
+    #         self._ops[(colname, 'max')] = self.by_amount_max
 
     q = conn.query(mapping)
 
@@ -111,17 +112,15 @@ def query_from_params(conn, mapping, **params):
     return q
 
 
-def load_fixtures_file(conn, fixtures_file, loader_func):
-    with open(fixtures_file) as fh:
-        fixtures = json.load(fh)
-        return load_fixtures(conn, fixtures, loader_func)
-
-
-def load_fixtures(conn, fixtures, loader_func):
-    conn.add_all([loader_func(x) for x in fixtures])
-
-
-def glob_to_like(x):
+def glob_to_like(x, wide=False):
     for (g, l) in (('*', '%'), ('.', '_')):
         x = x.replace(g, l)
+
+    if wide:
+        if not x.startswith('%'):
+            x = '%' + x
+
+        if not x.endswith('%'):
+            x = x + '%'
+
     return x
